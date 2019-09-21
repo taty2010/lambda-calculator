1572e43cfb5a56582fa1ea986036ae3a73057fa8
taty2010 <shydalys123@gmail.com>
2019-09-21 11:06:58 -0500

    passed the specials data from data.js and iterated over the numbers and push it into the buttons component

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,18 +1,25 @@
-import React from "react";
-import data from "./data"
-
+import React, {useState} from "react";
+import {specials} from "../../../data"
+import SpecialButton from "./SpecialButton"
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Specials = () => {
+const Specials = (props) => {
   // STEP 2 - add the imported data to state
-  import {numbes} from "../../../data"
+  const [specialState, setspecialsState] = useState(specials);
+  const SpecialsButtons = specials.map((buttonDataObject, i) => (
+    <SpecialButton key={i} buttonData={buttonDataObject} setspecialsState={setspecialsState} />
+ ));
+ console.log(SpecialsButtons)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {SpecialsButtons}
     </div>
   );
 };
+
+export default Specials
