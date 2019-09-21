2056d555960d7cce5df365d0eda88a158413c1a3
taty2010 <shydalys123@gmail.com>
2019-09-21 11:07:51 -0500

    copied props data from Specials into the button

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
+ const SpecialButton = (props) => {
   return (
-    <>
+    <button className="specialBttn buttons">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      <span>{props.buttonData}</span>
+    </button>
   );
 };
+
+export default SpecialButton
