662ac9a90d42fe4d04bf8b2f6a813d04f93cee61
taty2010 <shydalys123@gmail.com>
2019-09-22 15:23:15 -0500

    added the onClick function the the numberbuttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
-
-const NumberButton = () => {
+import Numbers from "./Numbers"
+const NumberButton = (props) => {
+  console.log(props)
   return (
-    <>
+    <button onClick={() => props.setNumberState(props)} className = "numbersBttn buttons">
+      <span>{props.buttonData}</span>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default NumberButton
