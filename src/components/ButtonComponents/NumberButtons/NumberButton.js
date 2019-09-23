153c819365e77e44247326b1ed0e3a1a60d888d3
taty2010 <shydalys123@gmail.com>
2019-09-23 17:40:28 -0500

    buttons were updated with

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Numbers from "./Numbers"
+
 const NumberButton = (props) => {
-  console.log(props)
+
   return (
-    <button onClick={() => props.setNumberState(props)} className = "numbersBttn buttons">
+    <button onClick={props.clickFunc} value={props.number} className = "numbersBttn buttons">
       <span>{props.buttonData}</span>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const OperatorButton = () => {
+
+const OperatorButton = (props) => {
   return (
-    <>
+    <button className= "operatorBttn buttons">
+      {props.buttonData}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default OperatorButton
