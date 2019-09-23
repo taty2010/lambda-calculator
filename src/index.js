1a324e394efe86bd9ce4e25bdce42ce2a3466027
taty2010 <shydalys123@gmail.com>
2019-09-23 17:26:40 -0500

    deleted unecessary import code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import data from "./data"
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
