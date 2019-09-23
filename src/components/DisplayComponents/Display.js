7ebb282ee4723d32026c9071b0691bbee15eb97e
taty2010 <shydalys123@gmail.com>
2019-09-23 17:30:01 -0500

    added props and inserted a template literal for the display state

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+
+const Display = (props) => {
+  console.log(props.display)
+  return <div className="display"><p>{`${props.display}`}</p>{/* Display any props data here */}</div>;
 };
+
+export default Display
