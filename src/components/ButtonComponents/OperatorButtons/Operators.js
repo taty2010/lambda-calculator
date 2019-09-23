4ffafebbfcaddd705e7dae7531f1161337f3e4fb
taty2010 <shydalys123@gmail.com>
2019-09-23 17:34:20 -0500

    removed useState and moved operators.map to the return section within the div

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,17 +1,24 @@
 import React from "react";
-import data from "./data"
-
+import {operators} from "../../../data"
+import OperatorButton from "./OperatorButton"
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  
+  //console.log(operatorButtons)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operators.map((buttonDataObject, i)=>(
+    <OperatorButton key={i} buttonData={buttonDataObject.char} />
+  ))}
     </div>
   );
 };
+
+export default Operators
