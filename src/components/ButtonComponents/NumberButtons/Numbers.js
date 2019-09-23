bfc348a334c45a670f78823c4eb28c399fc5db84
taty2010 <shydalys123@gmail.com>
2019-09-23 17:38:06 -0500

    removed useSate hook and moved the map function inside the div element amd declared number

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,6 @@
 import React from "react";
-import data from "./data"
+import {numbers} from "../../../data"
+import NumberButton from "./NumberButton.js"
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -8,13 +9,25 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+
+  /*const numberButtons = numbers.map((buttonDataObject, i) => (
+    <NumberButton number={buttonDataObject} key={i} buttonData={buttonDataObject} clickFunc={clickFunc} setNumberState={setNumberState} />
+  ));*/
+
+
   return (
-    <div>
+    
+    <div className="numberBttn-Container"> 
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numbers.map((buttonDataObject, i) => (
+    <NumberButton number={buttonDataObject} key={i} buttonData={buttonDataObject} clickFunc={props.clickFunc} />
+  ))}
     </div>
   );
 };
+
+export default Numbers
